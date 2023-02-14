cda6445eb4cfefe4d90d5b2f9bd63864bed5543b
Lisa Utsett <lisabethutsett@gmail.com>
2023-02-14 10:07:42 -0500

    fixed styling issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,14 @@ import Button from "react-bootstrap/Button";
 import bg from "../images/pexels-photo-416676.jpeg"
 
 const Home = () => (
-  <main>
+  <main className="home-container">
     <article className="image-with-bookingButton text-center">
-    <img className="home-image" src={bg} alt="surfer riding a huge wave"/>
-    <h1 className="home-title mb-5 mt-5">Are you ready for a Big Island Adventure?
+    <h1 className="home-title mb-5 mt-5">Are you ready for a Big Island Adventure?</h1>
     <Link to="/events">
       <Button className="book-now-button" variant="secondary">
         Book Now!
       </Button>
     </Link>
-    </h1>
     </article>
   </main>
 );
