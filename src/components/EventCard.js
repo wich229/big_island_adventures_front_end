36cf9236907fc9760fe96b9bdd98b0c9de961a2b
Lisa Utsett <lisabethutsett@gmail.com>
2023-02-16 03:08:05 -0500

    modified event info page

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -4,15 +4,13 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import './EventCard.css'
 import { Link,} from 'react-router-dom';
-import holderpic from "../images/pexels-diego-sandoval-4767081(2).jpg"
 
 
 const EventCard = ({tour}) => {
-
   return (
     <section>
-      <Card className='event-card' style={{ width: "18rem", height: "40rem"}}>
-        <Card.Img className='event-card-img' variant="top" src={holderpic} />
+      <Card className='event-card' style={{ width: "18rem", height: "35rem"}}>
+        <Card.Img className='event-card-img' variant="top" src={tour.photo_url} />
         <Card.Body>
           <Card.Title className="card-title">{tour.name}</Card.Title>
           <Card.Text>
--- src/pages/EventInfo.js
+++ src/pages/EventInfo.js
@@ -3,7 +3,8 @@ import '../App.css'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import SecNav from "../components/SecNav";
-import { Card } from "react-bootstrap";
+import { Card, Button} from "react-bootstrap";
+import './EventInfo.css'
 const kBaseUrl = process.env.REACT_APP_BACKEND_URL
 
 
@@ -11,6 +12,7 @@ const EventInfo = () => {
   const param = useParams();
   const page = "tour"
   const [tour, setTourState] = useState({});
+  
   useEffect(() => {
     axios
       .get(`${kBaseUrl}/tours/${param.id}`)
@@ -22,17 +24,25 @@ const EventInfo = () => {
       });
   
   }, [param.id]);
-  console.log(tour)
+
   return (
     <main>
         <section>
             <section>
                 <SecNav page={page}/>
             </section>
-            <section className="">
-              <Card variant="secondary">
-                <Card.Title>{tour.name}</Card.Title>
-                </Card>
+            <section >
+              <Card className="event-info" variant="secondary">
+                <Card.Img className="event-info-img" src={tour.photo_url}></Card.Img>
+                <section className="title-text-btn-container">
+                <Card.Title className="event-info-title">{tour.name}</Card.Title>
+                <Card.Text className="event-info-text">{tour.description}</Card.Text>
+                <section className="event-info-btns">
+                <Button variant="secondary" className="go-back-btn">Go Back</Button>
+                <Button variant="secondary" className="book-tour-btn">Book tour</Button>
+                </section>
+                </section>
+              </Card>
             </section>
         </section>
     </main>
