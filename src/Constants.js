b4264873dcd7542f210f550f86fe1c5a85b73d6a
Lisa Utsett <lisabethutsett@gmail.com>
2023-02-22 01:39:56 -0500

    changed backend ur;l

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,4 @@
-export const kBaseUrl = "http://big-island-be.herokuapp.com"
+// export const kBaseUrl = "http://big-island-be.herokuapp.com"
 export const apiKey = "KwEOp0fg9DesFJhrcSCVraGLNIvgkXHd"
 // export const kBaseUrl = 'http://localhost:5000'
+export const kBaseUrl = "http://be-big-island-adventures.herokuapp.com/"
